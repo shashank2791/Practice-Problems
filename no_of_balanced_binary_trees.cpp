682a0d161adb0a106ae3eefec8cdde7398787ab3
Shashank Madhwal <42387528+shashank2791@users.noreply.github.com>
2020-11-08 15:06:48 +0530

    Add files via upload

---
+++ no_of_balanced_binary_trees.cpp
@@ -0,0 +1,45 @@
+/*
+Given an integer h, find the possible number of balanced binary trees of height h.
+You just need to return the count of possible binary trees which are balanced.
+This number can be huge, so return output modulus 10^9 + 7.
+Write a simple recursive solution.
+Input Format :
+Integer h
+Output Format :
+Count % 10^9 + 7
+Input Constraints :
+1 <= h <= 40
+Sample Input 1:
+3
+Sample Output 1:
+15
+Sample Input 2:
+4
+Sample Output 2:
+315
+*/
+
+
+#include<bits/stdc++.h>
+
+
+int balancedBTs(int h){
+
+	if(h==0 || h==1){
+		return 1;
+	}
+
+	int m = pow(10,9) + 7;
+	int x = balancedBTs(h-1);
+	int y = balancedBTs(h-2);
+
+	long res1 = (long)x*x;
+	long res2 = (long)x*y*2;
+
+	int ans1 = (int)(res1%m);
+	int ans2 = (int)(res2%m);
+
+	int ans = (ans1+ans2)%m;
+
+	return ans;
+}
